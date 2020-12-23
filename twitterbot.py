2a8216a464b1863a6207b6114edd3b2db9db500c
anish-jampana <73216560+anish-jampana@users.noreply.github.com>
2020-12-23 12:37:22 -0800

    Update twitterbot.py

--- twitterbot.py
+++ twitterbot.py
@@ -12,20 +12,5 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-# txt file filled with my tweets (line by line, must be less than 280 char)
-file = open("/Users/anishjampana/Downloads/tweets.txt", "r") # replace tweets.txt with sample.txt or your own txt file.
-
-# reads line by line
-filelines = file.readlines()
-
-# closes once program reaches end of file
-file.close()
-
-# for loop to tweet each line by line every 24 hours (86400 seconds)
-for line in filelines:
-  print(line)
-  if line != '\n':
-      api.update_status(line)
-  else:
-      pass
-  sleep(86400)
+# add code from tweetfile.py here if you want to your robot to automatically tweet at certain time intervals
+# add code from follow.py here if you want to follow every account that follows you
